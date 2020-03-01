028340a45dfc280b657caa9ac88a6ec7e023c617
MahAbdulKhaliq <61640146+MahAbdulKhaliq@users.noreply.github.com>
2020-02-29 21:59:48 -0500

    Add files via upload

---
+++ NET/Lab5A/Lab5A/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Lab5A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.trackTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab5A.Properties.Resources.Faucet;
+            this.pictureBox1.Location = new System.Drawing.Point(56, 131);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 69);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(52, 80);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(104, 45);
+            this.trackBar.TabIndex = 1;
+            this.trackBar.Scroll += new System.EventHandler(this.TrackBar_Scroll);
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.Location = new System.Drawing.Point(53, 22);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(75, 23);
+            this.colorBtn.TabIndex = 2;
+            this.colorBtn.Text = "Colour";
+            this.colorBtn.UseVisualStyleBackColor = true;
+            this.colorBtn.Click += new System.EventHandler(this.ColorBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Location = new System.Drawing.Point(177, 22);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(75, 23);
+            this.closeBtn.TabIndex = 3;
+            this.closeBtn.Text = "Close";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // trackTimer
+            // 
+            this.trackTimer.Interval = 10;
+            this.trackTimer.Tick += new System.EventHandler(this.TrackTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(204, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(358, 528);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.colorBtn);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Bucket Filler 3000";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Button colorBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Timer trackTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NET/Lab5A/Lab5A/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+  Class:  Form1.cs
+  Author: Mahmood Abdul-Khaliq
+  Date:   December 6, 2019
+
+I, Mahmood Abdul-Khaliq, student number #000788833, certify that all code submitted is my own work; 
+                           that I have not copied it from any other source.  
+                           I also certify that I have not allowed my work to be copied by others.
+*/
+
+
+namespace Lab5A
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;                         //Encapsulates a GDI+ drawing surface
+        private Pen p;                              //Pens are used to draw objects
+        private SolidBrush b;                       //Brushes are used to fill graphics shapes
+        private Color c = Color.SkyBlue;            //Default colour for fluid
+        private int ticks;                          //Value used for time-related properties
+
+        private bool reset = false;                 //Used for resetting beaker when full
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+
+        }
+
+        /// <summary>
+        /// Paints initial lines for the 'bucket' and sets up Graphics object
+        /// with PaintEventHandler
+        /// </summary>
+        /// <param name="sender">Default sender</param>
+        /// <param name="e">Default PaintEventArgs</param>
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            p = new Pen(Color.White);
+            g.DrawLine(p, 300, 250, 300, 450);
+            g.DrawLine(p, 100, 250, 100, 450);
+            g.DrawLine(p, 100, 450, 300, 450);
+        }
+
+        /// <summary>
+        /// Gracefully closes the environment when the 'Exit' button is
+        /// clicked.
+        /// </summary>
+        /// <param name="sender">Default sender</param>
+        /// <param name="e">Default EventArgs</param>
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        /// <summary>
+        /// Changes the colour of the fluid to whatever the user
+        /// desires via a colorDialog prompt.
+        /// </summary>
+        /// <param name="sender">Default sender</param>
+        /// <param name="e">Default EventArgs</param>
+        private void ColorBtn_Click(object sender, EventArgs e)
+        {
+            //Confirms the dialog box works; then, switches the default colour to a user-selected one.
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                c = colorDialog.Color;
+            }
+        }
+
+        /// <summary>
+        /// Adds a stream from the faucet based off of the trackbar scroll progress.
+        /// Will hide the stream if the trackbar is set to 0 (and subsequently, stop the trackTimer object).
+        /// </summary>
+        /// <param name="sender">Default sender</param>
+        /// <param name="e">Default EventArgs</param>
+        private void TrackBar_Scroll(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            
+            //Resets the object once the bucket is full
+            if (reset)
+            {
+                b = new SolidBrush(Color.Black); //Sets up a brush with black colour to hide the previous filling
+                g.FillRectangle(b, 101, 249, 199, 200);
+                reset = false;
+            }
+            b = new SolidBrush(c); //Sets up a brush with selected colour
+          
+            //Stream position co-ordinates for open-faucet fluid. Used for simplicity
+            //as whent the faucet is shut off a black rectangle is drawn over the stream.
+            Rectangle streamPosition = new Rectangle(125, 200, 20, (250-ticks));
+
+            //Draws a stream based off of the selected colour and starts the trackTimer
+            if (trackBar.Value >= 1)
+            {                
+                g.FillRectangle(b, streamPosition);
+                trackTimer.Start();
+                trackTimer.Enabled = true;
+            }
+            //Draws a black rectangle over the stream and stops the trackTimer
+            else
+            {
+                b = new SolidBrush(Color.Black); //Sets up a brush with black colour to hide the previous stream
+                g.FillRectangle(b, streamPosition);
+                trackTimer.Stop();
+            }
+        }
+
+
+        /// <summary>
+        /// Controls all time-based commands, from flow-rate to current
+        /// beaker status.
+        /// 
+        /// This is all controlled via 'ticks' in the timer - an increased tick
+        /// leads to an increased value; this value determines increase with height
+        /// scaling of the fluid.
+        /// </summary>
+        /// <param name="sender">Default sender</param>
+        /// <param name="e">Default EventArgs</param>
+        private void TrackTimer_Tick(object sender, EventArgs e)
+        {
+            //Increases ticks and controls tick growth based off of trackBar value
+            ticks += 1 * (trackBar.Value);
+
+            //Stream position co-ordinates for open-faucet fluid. Used for simplicity
+            Rectangle streamPosition = new Rectangle(125, 200, 20, (250 - ticks));           g = this.CreateGraphics();
+            b = new SolidBrush(c); //Sets up a brush with selected colour to fill beaker with fluid
+
+            g.FillRectangle(b, 101, 450-ticks, 199, (trackBar.Value)); //Fills beaker with selected fluid; scales height and y-position appropriately based off of tick parameters
+
+            //Just before the beaker is filled: stops the flow, trackTimer, and forces a reset upon next fill attempt.
+            if (ticks >= 180)
+            {
+                trackTimer.Stop();
+                b = new SolidBrush(Color.Black);
+                g.FillRectangle(b, streamPosition);
+                trackBar.Value = 0;
+                ticks = 0;
+                reset = true;
+            }
+        }
+    }
+}
